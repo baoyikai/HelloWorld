3b547fb380eda1daa78d6b35c4ced262ebb4bae0
baoyikai <baoyikai2001@gmail.com>
2022-08-26 20:36:03 +0900

    no message

--- MTJavaWeb/fibMemo.java
+++ MTJavaWeb/fibMemo.java
@@ -1,33 +1,29 @@
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class fibMemo {
-	
-	static HashMap<Integer,Double> FibonacciCount = new HashMap<>();
-	
-	
+
 	public double fibCount(int key) {
-		FibonacciCount.put(0, 0.0);
-		FibonacciCount.put(1,1.0);
-		
-		if(key == 0) {
-			return 0;
-		}else if(key == 1) {
-			return 1;
-		}else {
-			for (int i = 2;i <= key;i++) {
-				FibonacciCount.put(i, FibonacciCount.get(i -1) + FibonacciCount.get(i - 2));
-				
-				
-			}
-			return FibonacciCount.get(key);
+			
+		if (key <= 1) {
+			return key;
 		}
-		
+		else {
+			double[] FibonacciCount = new double[key + 1];
+			FibonacciCount[0] = 0;
+			FibonacciCount[1] = 1;
+			for (int i = 2;i <= key;i++) {
+				FibonacciCount[i] = FibonacciCount[i - 1] + FibonacciCount[i - 2];	
+			}
+			System.out.println(Arrays.toString(FibonacciCount));
+			return FibonacciCount[key];
+			
+		}	
 	}
 
   public static void main(String[] args) {
-	  
 	  
 	  fibMemo fib = new fibMemo();
 	  System.out.println(fib.fibCount(0));
@@ -37,8 +33,7 @@ public class fibMemo {
 	  System.out.println(fib.fibCount(4));
 	  System.out.println(fib.fibCount(5));
 	  System.out.println(fib.fibCount(50));
-	  System.out.println(FibonacciCount);
-	  
+
 	  
   }
 
